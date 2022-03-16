020bc14707ae95013b85b8ccdee07e03d1e7d37a
Musti <96733958+xVares@users.noreply.github.com>
2022-03-16 07:16:27 +0100

    style: page icon, colored answer, hover/focus feat
    HTML
      - added page icon
    
    CSS
      - new hover and focus feat (border-radius)
      - reduced transition time to look more smooth
      - locked textarea horizontal scaling and set min height
      - added span style for string adjustment in app.js
    
    JSS
      - removed one conditional
      - added <span> el in conditional strings
      - added class for <span> so certain parts are now colored

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Lost Ark Currency Calculator
-Simple currency calculator for the Lost Ark game
+## What does it do?
+It calculates for you whether an item is worth to be purchased in the ***Auction House*** or in ***Marie's Shop*** by exchanging <ins>**Gold**</ins> (💰) into <ins>**Blue Crystals**</ins> (💎).
+
+---
+## How to use it
+### 1.) <ins>Enter the relevant values for the calculation</ins>
+* 💰 <ins>**Gold Value**</ins>
+
+  - Enter the current **💰price** of **95💎** which you can see in the ***Currency Exchange Menu***
+
+* 💎 <ins>**Blue Crystals**</ins>
+
+  - How much 💎 does your item cost in ***Marie's shop***?  
+   (e.g. ***20x Solar Grace*** cost **80💎**)
+
+* 🔨 <ins>**Auction Price**</ins>
+
+  - The **💰price** of the same amount of your item in the **Auction House**  
+  (e.g. if you can buy ***20x Solar Grace*** in ***Marie's Shop***, look up what ***20*** of them would cost you in the ***Auction House***)
+### 2.) <ins>Press the button!</ins>
--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// function for rule of three
+// function to calc worth of one bc
 let oneBcPrice = (gld) => {
 
   solution = gld / 95;
@@ -7,19 +7,18 @@ let oneBcPrice = (gld) => {
 }
 
 
-// onclick event for calc
+// on click event for calc
 document.getElementById("btn").addEventListener("click", (event) => {
 
   event.preventDefault();
 
-  // get form values
+  // get form input values
   let gold = document.getElementById("gold").value;
   let bc = document.getElementById("bc").value;
   let auction = document.getElementById("auction").value;
 
   // calculate actual gold price of bc value
   let price = oneBcPrice(gold) * bc;
-
   let conclusion = `You pay <span class="gold">${price.toFixed(1)} Gold</span> for <span class="crystals">${bc} Blue Crystals.`;
 
   // get <p>'s by ID
@@ -28,28 +27,21 @@ document.getElementById("btn").addEventListener("click", (event) => {
 
   answer.innerHTML = conclusion;
 
-  // check conditionals => which one is cheaper?
-
+  // conditionals
   if (auction <= 0 || price <= 0) {
 
-    let worth = `Please enter a valid value.`
+    let worth = `Please enter valid data.`
     recom.innerHTML = worth;
     answer.innerHTML = "";
   }
 
-  else if (auction < price) {
+  else if (auction <= price) {
 
-    let worth = `<span class="gold">Buy it with gold.</span>`
+    let worth = `<span class="gold">Buy your item with gold.</span>`
     recom.innerHTML = worth;
   }
 
-  else if (auction === price) {
-
-    let worth = `The price is the same it doesn't matter.`
-    recom.innerHTML = worth;
-  }
-
-  else {
+  else if (auction >= price) {
 
     let worth = `<span class="crystals">Buy it with Blue Crystals.</span>`
     recom.innerHTML = worth;
